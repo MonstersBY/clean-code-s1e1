a7a7efbbf6bb3904f1c300a169e56970cac2c5e4
Alex Romanchenko <monsters.alex0@gmail.com>
2022-10-31 14:03:27 +0300

    refactor: add alt for img; advanced rule 1.2

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="btn delete";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className='del-img';
+    deleteButtonImg.alt='delete';
     deleteButton.appendChild(deleteButtonImg);
 
 
